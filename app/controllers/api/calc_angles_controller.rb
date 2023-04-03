930496dd37a05cbedd674d54ccb0c5770540f160
E-Evgeniy <e.evgeniy.work@gmail.com>
2023-04-03 16:35:12 +0300

    work

--- app/controllers/api/calc_angles_controller.rb
+++ app/controllers/api/calc_angles_controller.rb
@@ -1,9 +1,68 @@
 module Api
   class CalcAnglesController < ApplicationController
-    def calculation
-      angle = ["30"]
+    L = 170
 
-      render( json: {angle: angle })
+    def calculation
+      angle = find_angle(params['wing1H1'], params['wing1H2'], params['wing2H1'],params['wing2H2'])
+
+      render(json: {angle: angle })
+    end
+
+    def find_angle(height11, height12, height21, height22)
+      return 'NONE' unless check_params(height11, height12, height21, height22)
+
+      height11 = height11.to_f
+      height12 = height12.to_f
+      height21 = height21.to_f
+      height22 = height22.to_f
+      params_wing1 = find_params_wing(height11, height12)
+      params_wing2 = find_params_wing(height21, height22)
+      find_alpha(params_wing1, params_wing2)
+    end
+
+    def find_params_wing(height1, height2)
+      result = {}
+      result['A'] = height2 - height1
+      result['B'] = find_x(height1, height2)
+      result['C'] = height1 * result['B']
+      result
+    end
+
+    def find_x(height1, height2)
+      c = height2 - height1
+      alpha = Math.asin(c / L)
+      (L * Math.cos(alpha)).round(7)
+    end
+
+    def find_alpha(params_wing1, params_wing2)
+      k1 = find_k1(params_wing1, params_wing2)
+      sqrt1 = find_sqrt(params_wing1)
+      sqrt2 = find_sqrt(params_wing2)
+      alpha_rad = Math.acos(k1 / (sqrt1 * sqrt2))
+      puts("alpha_rad #{alpha_rad}")
+      puts("((alpha_rad * 180) / Math::PI).round(2) #{((alpha_rad * 180) / Math::PI).round(2)}")
+      ((alpha_rad * 180) / Math::PI).round(2)
+    end
+
+    def find_k1(params_wing1, params_wing2)
+      a = params_wing1['A'] * params_wing2['A']
+      b = params_wing1['B'] * params_wing2['B']
+      (a - b).abs
+    end
+
+    def find_sqrt(params_wing)
+      a2 = params_wing['A'] * params_wing['A']
+      b2 = params_wing['B'] * params_wing['B']
+      Math.sqrt(a2 + b2)
+    end
+
+    def check_params(a, b, c, d)
+      puts("a.empty? #{a.empty?}")
+      if a.empty? || b.empty? || c.empty? || d.empty?
+        false
+      else
+        true
+      end
     end
   end
 end
--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -2,37 +2,65 @@ import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom/client"
 
 function App() {
-  const [loadedPlaces, setLoadedPlaces] = useState([])
+  const [angle, setAngle] = useState('None')
+  const [wing1H1, setwing1H1] = useState('')
+  const [wing1H2, setwing1H2] = useState('')
+  const [wing2H1, setwing2H1] = useState('')
+  const [wing2H2, setwing2H2] = useState('')
+  const [searchAngle, setSearchAngle] = useState('')
+  const INSERT_SPACE = '   '
+
 
   useEffect(() => {
     //Hit the server and get the places
 
-    const apiEndpoint = "/api/calc_angles"
+    const apiEndpoint = `api/calc_angles?wing1H1=${wing1H1}&wing1H2=${wing1H2}&wing2H1=${wing2H1}&wing2H2=${wing2H2}`
 
     fetch(apiEndpoint)
       .then(response => response.json())
       .then(data => {
-        setLoadedPlaces(data["angle"])
+        setAngle(data["angle"])
       }
       );
-  }, [])
+  }, [searchAngle])
+
+  const onSearchTextChangeW1H1 = (e) => {
+    setwing1H1(e.target.value);
+    setSearchAngle(e.target.value);
+  }
+
+  const onSearchTextChangeW1H2 = (e) => {
+    setwing1H2(e.target.value);
+    setSearchAngle(e.target.value);
+  }
+
+
+  const onSearchTextChangeW2H1 = (e) => {
+    setwing2H1(e.target.value);
+    setSearchAngle(e.target.value);
+  }
+
+  const onSearchTextChangeW2H2 = (e) => {
+    setwing2H2(e.target.value);
+    setSearchAngle(e.target.value);
+  }
+
 
   const fieldInput = (
 
     <div>
-      <div className="flex items-center justify-center p-12">
 
+      <div className="flex items-center justify-center p-2 bg-gray-200">
+        <div className="self-auto ...">
+          <img src="../images/wings.jpg" width="800" height="700"></img>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center p-8 bg-indigo-400">
+      
         <div className="mx-auto w-full max-w-[550px]">
           <form action="https://formbold.com/s/FORM_ID" method="POST">
-
-            <div>
-              <img
-                src="/assets/images/041.jpg"
-                class="h-auto max-w-full"
-                alt="..." />
-            </div>
-
-            <div className="mb-5 text-center block text-2xl text-base font-medium text-[#07074D]"> WING 1 </div>
+            <div className="mb-5 text-center block text-2xl text-base font-medium text-[#07074D] underline"> WING 1 </div>
             <div className="-mx-3 flex flex-wrap">
 
               <div className="w-full px-3 sm:w-1/2">
@@ -45,10 +73,12 @@ function App() {
                     H1 / mm
                   </label>
                   <input
-                    type="text"
+                    type="number"
+                    min="0"
                     name="wing1h1"
                     id="wing1h1"
                     placeholder="H1 / mm"
+                    onChange={onSearchTextChangeW1H1}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
@@ -62,16 +92,18 @@ function App() {
                     H2 / mm
                   </label>
                   <input
-                    type="text"
+                    type="number"
+                    min="0"
                     name="wing1h2"
                     id="wing1h2"
                     placeholder="H2 / mm"
+                    onChange={onSearchTextChangeW1H2}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
             </div>
-            <div className="mb-5 text-center block text-2xl text-base font-medium text-[#07074D]"> WING 2 </div>
+            <div className="mb-5 text-center block text-2xl text-base font-medium text-[#07074D] underline"> WING 2 </div>
             <div className="-mx-3 flex flex-wrap">
 
               <div className="w-full px-3 sm:w-1/2">
@@ -84,10 +116,12 @@ function App() {
                     H1 / mm
                   </label>
                   <input
-                    type="text"
+                    type="number"
+                    min="0"
                     name="wing1h1"
                     id="wing1h1"
                     placeholder="H1 / mm"
+                    onChange={onSearchTextChangeW2H1}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
@@ -101,30 +135,16 @@ function App() {
                     H2 / mm
                   </label>
                   <input
-                    type="text"
+                    type="number"
+                    min="0"
                     name="wing1h2"
                     id="wing1h2"
                     placeholder="H2 / mm"
+                    onChange={onSearchTextChangeW2H2}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
-            </div>
-            <div className="mb-5">
-              <label
-                htmlFor="guest"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                How many guest are you bringing?
-              </label>
-              <input
-                type="number"
-                name="guest"
-                id="guest"
-                placeholder="5"
-                min="0"
-                className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
             </div>
           </form>
         </div>
@@ -132,11 +152,19 @@ function App() {
     </div>
   )
 
-  return (
+  const findAngle = (
+    <div className="flex items-center justify-center p-2 mb-3 block text-2xl font-medium text-[#07074D]">
+      <div dangerouslySetInnerHTML={{__html: 'The resulting angle: &nbsp;'}} />
+        <div className="text-blue-600 text-2xl font-bold">
+         {angle}
+        </div>        
+    </div>
+  )
 
-    <div>The desired angle
-      {loadedPlaces}
+  return (
+    <div>
       {fieldInput}
+      {findAngle}
     </div>
   )
 }
